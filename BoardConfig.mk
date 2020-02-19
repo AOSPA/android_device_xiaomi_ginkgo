0e7e97f22769125e8a2dc498cbed49af6cd2b30b
dianlujitao <dianlujitao@lineageos.org>
2020-02-19 20:04:50 +0800

    ginkgo: Migrate to new DT2W node
    Change-Id: Ib7f33e2463caffdf18c5590911fc593b139f160c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_OTA_ASSERT_DEVICE := ginkgo,willow
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_WILLOW_FILES := $(DEVICE_PATH)/configs/hidl/manifest_willow.xml

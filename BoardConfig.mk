484fe91be6f661538e06b78ec33fddfbac8a0e7c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-12 23:13:40 +0530

    ginkgo: Switch to PA defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_ginkgo
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ginkgo
 
 # Kernel
-KERNEL_DEFCONFIG := vendor/ginkgo-perf_defconfig
+KERNEL_DEFCONFIG := vendor/pa_ginkgo-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 
 # NFC
